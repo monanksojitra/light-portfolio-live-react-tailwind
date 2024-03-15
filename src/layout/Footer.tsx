1f2d7e9a43581cd80378c60144042e05bec7d0ca
monank sojitra <monaksojitra4444@gmail.com>
2024-03-15 23:00:12 +0530

    remove code

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,4 +1,4 @@
-import React from "react"; 
+ 
 const Footer = () => { 
  return ( 
  <div className="w-full h-40 bg-black text-white text-4xl flex items-center justify-center"> 
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 const Navbar = () => {
   return (
     <div className="w-full h-40 bg-black text-white  text-4xl flex items-center justify-center">
